8105cc11b65bdb62211779b9881751672e225ea4
kidchang93 <lck930721@gmail.com>
2023-10-18 13:53:54 +0900

    스크립트 테스트 document.write으로 출력

--- JS_data/main.js
+++ JS_data/main.js
@@ -1,15 +1,17 @@
-'use strict';
+"use strict";
 // 구구단
 // for 문으로 a 의 값을 초기화 한 뒤 a의 범위는 10 미만의 수까지만 증가하며 돌기
 // a = 1일때 부터 b가 포함된 for문은 10 미만의 수까지 증가하며
 // 결과값인 result 를 통해 a의 현재 값과 b의 현재 값을
-// console.log 를 통해 출력
+// document.write 를 통해 출력
+
 for (let a = 1; a < 10; a++) {
-  for (let b = 1; b < 10; b++) {
-    let result = a * b;
-    console.log(a + ' * ' + b + ' = ' + result);
-  }
+	for (let b = 1; b < 10; b++) {
+		let result = a * b;
+		document.write(a + " * " + b + " = " + result + "<br />");
+	}
 }
+
 // 등급 구하기
 // jumSu 변수에 값을 대입.
 let jumSu = 65;
@@ -22,31 +24,32 @@ let finalJumSu = jumSu - naMuGi;
 // 각 등급에 맞는 입력값에 대한 출력값이 나오면
 // break 문을 통해 더이상 반복문을 돌지 않는다.
 switch (finalJumSu) {
-  case 90:
-    console.log('A등급입니다.');
-    break;
-  case 80:
-    console.log('B등급 입니다');
-    break;
-  case 70:
-    console.log('C등급 입니다');
-    break;
-  case 60:
-    console.log('D등급 입니다.');
-    break;
-  default:
-    console.log('F등급 입니다.');
-    break;
+	case 90:
+		document.write("A등급입니다.<br />");
+		break;
+	case 80:
+		document.write("B등급 입니다.<br />");
+		break;
+	case 70:
+		document.write("C등급 입니다.<br />");
+		break;
+	case 60:
+		document.write("D등급 입니다.<br />");
+		break;
+	default:
+		document.write("F등급 입니다.<br />");
+		break;
 }
+
 // 3개의 값중 가운데 값 구하기.
 // arr 에 배열 변수를 초기화
 // 가운데 값은 0,1,2 중 1이므로 1을 출력.
 const arr = [1, 2, 3];
-console.log(arr[1]);
+document.write(arr[1] + "<br />");
 
 // 삼각형 모양 별찍기
 // ret에는 빈문자열로 변수를 초기화.
-let ret = '';
+let ret = "";
 // size 에는 5라는 홀수 값을 대입.
 let size = 5;
 
@@ -55,62 +58,51 @@ let size = 5;
 // ret에 공백을 더한값을 넣고 j 를 1 증가시킨 뒤
 // 세 번째 for문으로 들어간다.
 for (let i = 1; i <= size; i++) {
-  for (let j = 1; j <= size - i; j++) {
-    ret += ' ';
-  }
-  // 공백이 더해진 상태에 k 라는 변수를 초기화 하고
-  // k 의 값이 2 * i-1 보다 작을 경우
-  // ret의 값에 *를 더한 값을 출력한 뒤 k 를 1 증가시키고
-  // 첫번째 for문으로 들어가서 반복 작업을 수행한다.
-  for (let k = 1; k <= 2 * i - 1; k++) {
-    ret += '*';
-  }
-  // i의 첫번째 반복이 마치게 되면 한줄 띄어서 출력한다.
-  ret += '\n';
+	for (let j = 1; j <= size - i; j++) {
+		ret += " ";
+	}
+	// 공백이 더해진 상태에 k 라는 변수를 초기화 하고
+	// k 의 값이 2 * i-1 보다 작을 경우
+	// ret의 값에 *를 더한 값을 출력한 뒤 k 를 1 증가시키고
+	// 첫번째 for문으로 들어가서 반복 작업을 수행한다.
+	for (let k = 1; k <= 2 * i - 1; k++) {
+		ret += "*";
+	}
+	// i의 첫번째 반복이 마치게 되면 한줄 띄어서 출력한다.
+	ret += "<br/>";
 }
-
-// for (let i = size - 1; i > 0; i--) {
-//   for (let j = size; j > i; j--) {
-//     ret += ' ';
-//   }
-//   for (let k = 1; k <= 2 * i - 1; k++) {
-//     ret += '*';
-//   }
-//   ret += '\n';
-// }
-
-console.log(ret);
+document.write(ret);
 
 // 10 개의 값 중에서 가운데 값 구하기
 // function 으로 함수 클래스를 구현한다.
 function minOfArray(nums) {
-  // nums의 배열에서 최소값을 찾기 위해 Infinity 로 초기화를 한다.
-  let min = Infinity;
-  // for문을 사용해 배열의 가장 최소값을 반환한다.
-  for (let i = 0; i < nums.length; i++) {
-    if (min > nums[i]) {
-      min = nums[i];
-    }
-  }
-  return min;
+	// nums의 배열에서 최소값을 찾기 위해 Infinity 로 초기화를 한다.
+	let min = Infinity;
+	// for문을 사용해 배열의 가장 최소값을 반환한다.
+	for (let i = 0; i < nums.length; i++) {
+		if (min > nums[i]) {
+			min = nums[i];
+		}
+	}
+	return min;
 }
 // for문을 사용해 배열의 가장 최대값을 반환한다.
 function maxOfArray(nums) {
-  // 반복문에서 최대값을 찾기위해 음의 무한대로 초기화 한다.
-  let max = -Infinity;
-  for (let i = 0; i < nums.length; i++) {
-    if (max < nums[i]) {
-      max = nums[i];
-    }
-  }
-  return max;
+	// 반복문에서 최대값을 찾기위해 음의 무한대로 초기화 한다.
+	let max = -Infinity;
+	for (let i = 0; i < nums.length; i++) {
+		if (max < nums[i]) {
+			max = nums[i];
+		}
+	}
+	return max;
 }
 // 배열 대입
 const nums = [11, 123, 27, 49, 63, 85, 42, 72, 51, 36];
 // 배열의 오름차순으로 정렬
 const sortedNums = nums.sort((a, b) => a - b);
 // 정렬된 배열을 출력
-console.log(sortedNums);
+document.write(sortedNums + "<br/>");
 // 최소값을 구한다.
 const min = minOfArray(nums);
 // 최대값을 구한다.
@@ -119,20 +111,20 @@ const max = maxOfArray(nums);
 let sum = 0;
 // for문을 돌며 배열의 모든 값의 합을 구한다.
 for (let i = 0; i < nums.length; i++) {
-  sum += nums[i];
+	sum += nums[i];
 }
 // middle 이라는 변수에 정렬된 nums의 중간 값을 구한다.
 const middle = (nums[4] + nums[5]) / 2;
 // 중간값 출력
-console.log(middle);
+document.write(middle + "<br/>");
 // 모든 합 출력
-console.log(sum);
+document.write(sum + "<br/>");
 // 최소값 출력
-console.log(min);
+document.write(min + "<br/>");
 // 최대값 출력
-console.log(max);
+document.write(max + "<br/>");
 
 // const min = Math.min.apply(null, nums);
 // const max = Math.max.apply(null, nums);
-// console.log(min);
-// console.log(max);
+// document.write(min);
+// document.write(max);
